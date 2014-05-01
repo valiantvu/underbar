8e33796b21dffb1ba2e2f39204b5c802c1c0b43d
valiantvu <michelle.thi.vu@gmail.com>
2014-05-01 01:00:08 -0700

    Completed invoke for function reference

--- src/underbar.js
+++ src/underbar.js
@@ -185,6 +185,21 @@ var _ = {};
   // Calls the method named by methodName on each value in the list.
   // Note: you will nead to learn a bit about .apply to complete this.
   _.invoke = function(collection, functionOrKey, args) {
+
+      var result = [];
+
+      for (var key in collection){
+
+        if (typeof functionOrKey == 'function'){
+          result.push(functionOrKey.apply(collection[key],args));
+        }
+        else{
+          result.push(collection[key].functionOrKey(args));
+        }
+
+      }
+
+      return result;
   };
 
   // Reduces an array or object to a single value by repetitively calling
